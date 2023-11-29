a7b437a946954c649c1ef82ee57b17d2ec836b4c
Guilherme Vaz <guivs56@gmail.com>
2023-11-29 01:27:57 -0300

    project

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { StyleSheet, Text, View } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import DeputadoStack from './screens/deputados/DeputadoStack';
+import Noticias from './screens/noticias/Noticias';
+
+const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <PaperProvider>
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName='Disciplinas'>
+          <Tab.Screen
+            name="Deputados" 
+            component={DeputadoStack}
+            options={{
+              tabBarIcon: () => (
+                <MaterialCommunityIcons name="bookshelf" size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Noticias" 
+            component={Noticias}
+            options={{
+              tabBarIcon: () => (
+                <MaterialCommunityIcons name="book-open-variant" size={26} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  </>
   );
 }
 
---
+++ screens/noticias/Noticias.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { StyleSheet, Button, View } from 'react-native';
+import { Video } from 'expo-av';
+
+const Noticias = () => {
+
+ const video = React.useRef(null);
+ const [status, setStatus] = React.useState({});
+
+  return (
+    <View style={styles.container}>
+    <Video
+      ref={video}
+      style={styles.video}
+      source={require("./agenda.mp4")}
+      useNativeControls
+      resizeMode="contain"
+      isLooping
+      onPlaybackStatusUpdate={setStatus}
+    />
+    <View style={styles.buttons}>
+      <Button title="Play from 5s" onPress={() => video.current.playFromPositionAsync(5000)} />
+      <Button title={status.isLooping ? "Set to not loop" : "Set to loop"} onPress={() => video.current.setIsLoopingAsync(!status.isLooping)} />
+    </View>
+    <StatusBar style="auto" />
+  </View>
+  );
+}
+
+export default Noticias
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    video: {
+      flex: 1,
+      alignSelf: 'stretch'
+    },
+    buttons: {
+      margin: 16
+    }
+  });
+  
